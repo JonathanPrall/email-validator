cb8142efe0ff701e95cbc8c2da6289cf7838e7eb
JonathanPrall <jn219969@dal.ca>
2017-03-02 13:28:43 -0400

    Push Initial code
    Upload the original project with no changes.

---
+++ src/main/java/cs/dal/csci3130/jprall/email_validator/App.java
@@ -0,0 +1,13 @@
+package cs.dal.csci3130.jprall.email_validator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
